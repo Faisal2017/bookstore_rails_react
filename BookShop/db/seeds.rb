6be0caad10eb3ba767632d64305b7253f1a099ee
Faisal <faisal_a@live.co.uk>
2017-09-06 14:25:00 +0100

    edit button added to book.jsx

--- BookShop/db/seeds.rb
+++ BookShop/db/seeds.rb
@@ -18,6 +18,9 @@ g4 = Genre.create({ name: "Mystery" })
 g5 = Genre.create({ name: "Classical" })
 g6 = Genre.create({ name: "Crime" })
 
+g7 = Genre.create({ name: "Romance" })
+g8 = Genre.create({ name: "Horror" })
+
 a1 = Author.create({ name: "Patrick Rothfuss" })
 a2 = Author.create({ name: "David Gunn" })
 
@@ -26,6 +29,9 @@ a4 = Author.create({ name: "Dan Brown"})
 
 a5 = Author.create({ name: "Charles Dickens"})
 a6 = Author.create({ name: "Thomas Harris"})
+
+a7 = Author.create({ name: "JR Ward"})
+a8 = Author.create({ name: "Stephen King"})
 
 book1 = Book.create({ title: "The Name of the Wind", quantity: 10, genre_id: g1.id, author_id: a1.id})
 
@@ -37,4 +43,8 @@ book4 = Book.create({ title: "Angels & Demons", quantity: 16, genre_id: g4.id, a
 
 book5 = Book.create({ title: "A Tale of Two Cities", quantity: 7, genre_id: g5.id, author_id: a5.id})
 
-book5 = Book.create({ title: "Red Dragon", quantity: 13, genre_id: g6.id, author_id: a6.id})
+book6 = Book.create({ title: "Red Dragon", quantity: 13, genre_id: g6.id, author_id: a6.id})
+
+book7 = Book.create({ title: "Lover At Last", quantity: 11, genre_id: g7.id, author_id: a7.id})
+
+book8 = Book.create({ title: "Doctor Sleep", quantity: 11, genre_id: g8.id, author_id: a8.id})
